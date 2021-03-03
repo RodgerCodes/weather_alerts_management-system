b5b5d3fa5cf554ec25122f1461b2d85dd9ca1945
Rodger Kumwanje <rkumwanje@gmail.com>
2021-03-03 13:07:09 +0000

    register ul and validation

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('Auth.register');
+    }
+
+    public function store(Request $request){
+        // validate user;
+        $this->validate($request, [
+            'name'=>'required|max:255',
+            'username'=>'required',
+            'email'=>'required|email',
+            'role'=>'required',
+            'password'=>'required'
+        ]);
+    }
+}
